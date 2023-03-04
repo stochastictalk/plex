670988196448ddafb01281ec89704d04714795b1
kaplan <jerome.wynne18@ic.ac.uk>
2023-03-04 20:38:40 +0000

    Update name.

--- src/plex/__init__.py
+++ src/plex/__init__.py
@@ -1,7 +1,3 @@
-"""The mind-blowing main package!"""
-
-from ._get_hello import get_hello
-
 try:
     from ._version import version as __version__
     from ._version import version_tuple
@@ -10,5 +6,4 @@ except ImportError:
     version_tuple = (0, 0, "unknown version")
 
 __all__ = [
-    "get_hello",
 ]
